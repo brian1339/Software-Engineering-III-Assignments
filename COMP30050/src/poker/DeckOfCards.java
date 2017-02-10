14c37854f258a00d7e0b3b2b389f52f403c2a2aa
13394546 <brian.dunne.2@ucdconnect.ie>
2017-02-10 19:36:11 +0000

    Implemented shuffle() reset() and dealNext()

--- COMP30050/src/poker/DeckOfCards.java
+++ COMP30050/src/poker/DeckOfCards.java
@@ -1,31 +1,62 @@
 package poker;
 
 import poker.PlayingCard;
+
 import java.util.concurrent.Semaphore;
 import java.util.Random;
 
 public class DeckOfCards {
-	
-	private PlayingCard[] cards;
+
+	private int cardsDealt;
+	private PlayingCard[] deck;
 	private Semaphore dealAvailable;
 	
 	//TODO
 	public DeckOfCards(){
-		
+		reset();
+		shuffle();
+		dealAvailable = new Semaphore(1);
 	}
 	
 	//TODO
-	public PlayingCard dealNext(){
-		
+	public PlayingCard dealNext() throws InterruptedException{
+		dealAvailable.acquire();
+		PlayingCard outputCard = deck[cardsDealt];
+		cardsDealt++;
+		dealAvailable.release();
+		return outputCard;
 	}
 	
 	//TODO
+	public void returnCard(PlayingCard discarded){
+		
+	}
+	
+	/*
+	 * Shuffles the deck by randomly selecting two card indexes in the deck array and swapping the cards
+	 * This is repeated by the size of the deck squared times to ensure the deck is thoroughly shuffled
+	 */
 	public void shuffle(){
 		Random rand = new Random(System.currentTimeMillis());
+		for(int i=0; i<(deck.length*deck.length); i++){
+			int index1, index2;
+			do {
+				index1 = rand.nextInt(deck.length);
+				index2 = rand.nextInt(deck.length);
+			} while (index1 != index2);
+			
+			PlayingCard temp = deck[index1];
+			deck[index1] = deck[index2];
+			deck[index2] = temp;
+		}
 	}
 	
+	/*
+	 * Creates a new deck of cards array and sets cards dealt to zero
+	 */
 	public void reset(){
-		
+		deck = PlayingCard.newFullPack();
+		cardsDealt = 0;
 	}
 	
 	//TODO
