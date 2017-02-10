7be706a805adead51973b03323ab1078f4ca544f
13394546 <brian.dunne.2@ucdconnect.ie>
2017-02-10 18:48:15 +0000

    Set up method names wiht return types

---
+++ COMP30050/src/poker/DeckOfCards.java
@@ -0,0 +1,37 @@
+package poker;
+
+import poker.PlayingCard;
+import java.util.concurrent.Semaphore;
+import java.util.Random;
+
+public class DeckOfCards {
+	
+	private PlayingCard[] cards;
+	private Semaphore dealAvailable;
+	
+	//TODO
+	public DeckOfCards(){
+		
+	}
+	
+	//TODO
+	public PlayingCard dealNext(){
+		
+	}
+	
+	//TODO
+	public void shuffle(){
+		Random rand = new Random(System.currentTimeMillis());
+	}
+	
+	public void reset(){
+		
+	}
+	
+	//TODO
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
